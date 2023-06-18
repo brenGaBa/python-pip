4356a6514c64638b0503510ebf81aa88de5a85fc
brenda <bren.egaba18@gmail.com>
2023-06-17 19:25:54 -0600

    fix typo

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def run():
     data =list(filter(lambda item: item['Continent'] == 'South America', data))
 
     countries = list(map(lambda x: x['Country'], data))
-    percentages = list(map(lambda x: x['Word Population Percentage'], data))
+    percentages = list(map(lambda x: x['World Population Percentage'], data))
     charts.generate_pie_chart(countries, percentages)
     
     country = input('Type country => ')
